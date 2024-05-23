ddd8bf94fa28179b0b11d08172a82236b9ec882c
Tamm2 <totukakidikuto2525@gmail.com>
2024-05-23 20:06:26 +0900

    change database

--- intern/settings.py
+++ intern/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'intern.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat_app',
+        'USER': 'ikuta',
+        'PASSWORD': 'chatapptamura',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
