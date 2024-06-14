6626d716e98eed6f2436aa46e0f0536841d2d2a9
Tamm2 <totukakidikuto2525@gmail.com>
2024-06-14 16:31:34 +0900

    kadai2 done

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('signup/', views.signup_view, name='signup_view'),
-    path('login', views.Login.as_view(), name='login_view'),
     path('friends', views.friends, name='friends'),
     path('talk_room/<int:user_id>', views.talk_room, name='talk_room'),
     path('setting', views.setting, name='setting'),
